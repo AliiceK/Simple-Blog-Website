6de4803d653664c52b6cd434cba21a8299a5c8af
AliiceK <112477235+AliiceK@users.noreply.github.com>
2023-06-07 10:20:49 +0300

    Wrapped up with the delete function

--- app.js
+++ app.js
@@ -111,16 +111,28 @@ app.post('/blogs', (req,res) => {
     })
 })
 
-app.get('/blogs:id', (req,res) => {
-    const id = req.paramater.id;
+app.get('/blogs/:id', (req,res) => {
+    const id = req.params.id;
     Blog.findById(id)
     .then((result) => {
-        res.render('details', {blog: title, title : 'Blog Details'}); 
+        res.render('details', {blog: result, title : 'Blog Details'}); 
     })
     .catch(err => {
         console.log(err);
     })
 })
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    
+    Blog.findByIdAndDelete(id)
+      .then(result => {
+        res.json({ redirect: '/blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
 
 app.get('/about', (req,res)=> {
     // res.sendFile('./views/about.html', {root: __dirname});
